78f5bfba3b70c0b207780d5dea4ed7a06da4c530
David Kuoch <kuoch0305@gmail.com>
2020-08-25 16:31:29 +0200

    add photo to seed, delete global navbar && set_event in event controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,4 +1,11 @@
 class EventsController < ApplicationController
+
+    before_action :set_event, only: [ :show ]
+
+  def show
+    @event_image = @event.sport.photo
+  end
+
   def new
     @sport = Sport.all
     @event = Event.new
@@ -14,6 +21,11 @@ class EventsController < ApplicationController
   end
 
 private
+
+  def set_event
+    @event = Event.find(params[:id])
+    #authorize @event  # For Pundit
+  end
 
   def event_params
     params.require(:event).permit(:title, :location, :description, :start_time, :number_of_participants, :sport_id)
