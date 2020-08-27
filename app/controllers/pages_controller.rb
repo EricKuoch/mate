78aaa0ad819c35a37e09940d003be100bffdc31a
Eric KUOCH <ek.kuoch@gmail.com>
2020-08-27 13:18:27 +0200

    add map to Home

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,14 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
   def home
+    @events = Event.all
+    # the `geocoded` scope filters only events with coordinates (latitude & longitude)
+    @markers = @events.geocoded.map do |event|
+      {
+        lat: event.latitude,
+        lng: event.longitude,
+      }
+    end
   end
 
   def dashboard
