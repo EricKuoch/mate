e9dfc04812a13a084f7b1f8250aa1a2287049050
Eric KUOCH <ek.kuoch@gmail.com>
2020-08-28 09:18:19 +0200

    changed seed and improve the profile

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -29,7 +29,7 @@ import "bootstrap";
 // import { initSelect2 } from '../components/init_select2';
 import { initForm, changeStep } from '../plugins/init_form';
 import { initMapbox } from '../plugins/init_mapbox';
-
+import { initProfile } from '../plugins/init_profile';
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
@@ -37,5 +37,6 @@ document.addEventListener('turbolinks:load', () => {
   if(document.querySelector('.form .step.active')){
     changeStep()
   }
+  initProfile()
   initMapbox()
 });
---
+++ app/javascript/plugins/init_profile.js
@@ -0,0 +1,17 @@
+const initProfile = () => {
+  const events = document.querySelector('.event-title');
+  console.log(events)
+  if (events) (
+    events.forEach((event) => {
+      addEventListener('click', (event) => {
+
+        if(event.style.display === "none"){
+          event.style.display = "block";
+        } else {
+          event.style.display = "none";
+        }
+      })
+    })
+  )
+}
+export { initProfile };
