7d60a9b6bd369184c39cc81fc1c995a50dfafe3f
Léo Pictet <leo@pictet.net>
2020-08-31 15:26:45 +0200

    First steps to choose date

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -34,6 +34,7 @@ import { initDashboard} from '../plugins/init_dashboard';
 import { initBurger } from '../plugins/init_burger';
 import { initPopup } from '../plugins/init_popup';
 import { initModal } from '../plugins/init_modal';
+
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
