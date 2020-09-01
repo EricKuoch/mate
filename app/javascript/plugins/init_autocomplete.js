132359fd38e0fb7491b1dbfede02ec74629b538b
Léo Pictet <leo@pictet.net>
2020-09-01 17:46:56 +0200

    removing auto-complete search on home

--- app/javascript/plugins/init_autocomplete.js
+++ app/javascript/plugins/init_autocomplete.js
@@ -1,15 +1,11 @@
 import places from 'places.js';
 
 const initAutocomplete = () => {
-  const addressInput = document.getElementById('query_location');
-  if (addressInput) {
-    places({ container: addressInput });
-  }
   const evenLocation = document.getElementById('event_location');
   if (evenLocation) {
     places({ container: evenLocation });
   }
-  
+
 };
 
 export { initAutocomplete };
