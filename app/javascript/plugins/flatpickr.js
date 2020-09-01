57a392783d19acf65a3eac2a9473cdaec5a19e24
Narcisse Bosso <narcissebosso@yahoo.fr>
2020-09-01 09:36:24 +0200

    flatpickr fixed

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -1,6 +1,12 @@
 import flatpickr from "flatpickr";
 
-flatpickr(".datepicker", {
-  enableTime: true,
-  dateFormat: "Y-m-d H:i",
-});
+const datepicker = () => {
+  flatpickr(".datepicker", {
+    enableTime: true,
+    dateFormat: "Y-m-d H:i",
+    disableMobile: "true"
+  });
+}
+
+
+export {datepicker};
