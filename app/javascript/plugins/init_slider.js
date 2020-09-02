c657c77811799fed0254639a8322f1f7daacc71b
Léo Pictet <leo@pictet.net>
2020-09-02 10:16:58 +0200

    problem resolvded

--- app/javascript/plugins/init_slider.js
+++ app/javascript/plugins/init_slider.js
@@ -1,11 +1,12 @@
 const initSlider =() => {
+  if (document.getElementById('customRange11')) {
     const valueSpan = document.querySelector('.valueSpan2');
     const value = document.getElementById('customRange11');
     valueSpan.innerHTML = value.value;
     value.addEventListener('change', (event) => {
         valueSpan.innerHTML = value.value;
         console.log(value);
-
     })
+  }
 }
 export{ initSlider };
